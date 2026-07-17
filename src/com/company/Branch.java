01e1e987a99224232650d7722d15ee439dacc9ae
Bashar <salmobeschir@gmail.com>
2026-07-17 20:51:53 +0200

    branch class initialized

--- src/com/company/Branch.java
+++ src/com/company/Branch.java
@@ -1,4 +1,49 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Branch {
+    private String name;
+    private ArrayList<Customer> customers;
+
+    public Branch(String name) {
+        this.name = name;
+        customers = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Customer> getCustomers() {
+        return customers;
+    }
+    public boolean newCustomer(String name, double money) {
+        if (findCustomer(name)==null) {
+            customers.add(new Customer(name, money));
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean addCustomerTransaction(String name, double transaction) {
+        Customer customer = findCustomer(name);
+        if (customer != null) {
+            customer.addTransaction(transaction);
+            return true;
+        }
+        return false;
+    }
+
+    private Customer findCustomer(String name) {
+        for (Customer customer : customers) {
+            if (customer.getName().equalsIgnoreCase(name))  {
+                return customer;
+            }
+        }
+
+        return null;
+    }
 }
+
