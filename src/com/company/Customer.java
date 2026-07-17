7a59691d4a193992e001a279cb6ae98df0003ba5
Bashar <salmobeschir@gmail.com>
2026-07-17 20:39:26 +0200

    customer class initialized

--- src/com/company/Customer.java
+++ src/com/company/Customer.java
@@ -1,4 +1,28 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Customer {
+
+    private String name;
+    private ArrayList<Double> transactions;
+
+    public Customer(String name, double initialTransaction) {
+        this.name = name;
+        this.transactions = new ArrayList<>();
+        this.transactions.add(initialTransaction);
+    }
+
+public String getName() {
+    return name;
+}
+
+public ArrayList<Double> getTransactions() {
+    return transactions;
+}
+
+public void addTransaction(Double transaction) {
+    this.transactions.add(transaction);
+}
+
 }
