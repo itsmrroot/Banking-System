c2de3146cccf9b41f4a26eb30866cf27b3205eea
Bashar <salmobeschir@gmail.com>
2026-07-19 18:16:59 +0200

    bank class initialized

--- src/com/company/Bank.java
+++ src/com/company/Bank.java
@@ -1,4 +1,72 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Bank {
+    private String name;
+    private ArrayList<Branch> branches;
+
+    public Bank(String name) {
+        this.name = name;
+        branches = new ArrayList<>();
+    }
+    public boolean addBranch(String branchName) {
+        if (findBranch(branchName) == null) {
+            branches.add(new Branch(branchName));
+            return true;
+        }
+
+        return false;
+
+    }
+    public boolean addCustomer(String branchName, String customerName, double initialTransaction) {
+        Branch branch = findBranch(branchName);
+        if (branch != null) {
+            return branch.newCustomer(customerName, initialTransaction);
+        }
+        return false;
+    }
+    public boolean addTransaction(String branchName, String customerName, double initialTransaction) {
+        Branch branch = findBranch(branchName);
+        if (branch != null) {
+            return branch.addCustomerTransaction(customerName, initialTransaction);
+        }
+        return false;
+    }
+    private Branch findBranch(String branchName) {
+        for (Branch branch : branches) {
+            if (branch.getName().equalsIgnoreCase(branchName)) {
+                return branch;
+            }
+        }
+        return null;
+    }
+    public boolean listCustomers(String branchName, boolean printTransactions) {
+        Branch branch = findBranch(branchName);
+        if (branch != null) {
+            System.out.println("Customer details for branch " + branch.getName());
+            ArrayList<Customer> branchCustomers = branch.getCustomers();
+            for (int i = 0; i < branchCustomers.size(); i++) {
+                Customer customer = branchCustomers.get(i);
+                System.out.println("Customer: " + customer.getName() + "[" + (i + 1) + "]");
+                if (printTransactions) {
+                    System.out.println("Transactions");
+                    ArrayList<Double> transactions = customer.getTransactions();
+                    for (int j = 0; j < transactions.size(); j++) {
+                        System.out.println("[" + (j + 1) + "] Amount " + transactions.get(j));
+                    }
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Bank{" +
+                "name='" + name + '\'' +
+                ", branches=" + branches +
+                '}';
+    }
 }
